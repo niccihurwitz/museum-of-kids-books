40ada1998323505ffe1e1fc5007d5395086226df
Rosalind <ros.skinner@gmail.com>
2020-11-10 10:46:47 +1100

    add margin to give height to loading images

--- src/components/book.js
+++ src/components/book.js
@@ -30,6 +30,7 @@ export default function BookPage (props) {
     const data = getBook(store.getState(), `/${props.match.params.id}`)
     if (data !== book) {
       setBook(data)
+      setloaded(true)
     }
   }
   useEffect(() => {
@@ -92,9 +93,10 @@ export default function BookPage (props) {
               const src = require(`../books${book.permalink}/${item.file}`).default
               return (
                 <div className={`mv4 justify-between pl0 pl4-l ${width} relative`} key={key}>
-                  <div className='absolute placeholder' style={{backgroundColor: book.color}}/>
-                  <img className='w-100 o-0 lazy-image' onLoad={onLoad} src={src} />
-                  
+                  <div className='w-100 relative image-container'>
+                    <div className='absolute placeholder' style={{backgroundColor: book.color}}/>
+                    <img className='w-100 o-0 lazy-image' onLoad={onLoad} src={src} />
+                  </div>
                   {item.caption && <div className='mt3 mh4 mh0-l f5 lh-copy'>{item.caption}</div>}
                 </div>
               )
